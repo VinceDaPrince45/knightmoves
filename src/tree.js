33aa818bb288fa5821d7c3fec6de429c27eb5686
Vincent <Vincentnguyen030@gmail.com>
2023-07-20 20:05:15 -0500

    Write algorithm to return array of path taken

--- src/tree.js
+++ src/tree.js
@@ -51,7 +51,6 @@ class Tree {
                 for (const node of checkList) {
                     if (node.data !== null) {
                         if (node.data[0] == finalPosition[0] && node.data[1] == finalPosition[1]) {
-                            console.log(node);
                             return;
                         }
                     }
@@ -69,10 +68,51 @@ class Tree {
             return newPosition;
         }
     }
+
+    shortestPath(array=[],queue=[this.root],length=0) {
+        // use algorithm to find shortest path
+        // use level order traversal
+        while (queue.length > 0) {
+            let results = [];
+            for (let i=0;i<(8**length);i++) {
+                const currentNode=queue.shift();
+                if (currentNode == null) {
+                    results.push(null)
+                    for (let i = 0;i<8;i++) {
+                        queue.push(null);
+                    }
+                    continue;
+                };
+                results.push(currentNode.data);
+                queue.push(currentNode.leftFour,currentNode.leftThree,currentNode.leftTwo,currentNode.leftOne,currentNode.rightOne,currentNode.rightTwo,currentNode.rightThree,currentNode.rightFour);
+            }
+            
+            array[length] = results;
+            for (const item of results) {
+                if ((item !== null) && (item[0] == this.end[0] && item[1] == this.end[1])) {
+                    // find index of current result
+                    // since idx will always be in array[length], or the last array of the array
+                    // go to array[length - 1] and use idx
+                    let path = [];
+                    path.push(item);
+                    let idx = results.indexOf(item)
+                    length--;
+                    while (length >= 0) {
+                        idx = Math.trunc(idx/8);
+                        path.push(array[length--][idx])
+                    }
+                    console.log(path.reverse())
+                    return;
+                }
+            }
+            length++;
+        }
+    }
 }
 
 export function runCheck() {
-    const tree = new Tree([0,0],[4,5]);
+    const tree = new Tree([3,3],[4,3]);
     tree.buildTree();
-    console.log(tree)
+    console.log(tree.root)
+    tree.shortestPath();
 }
